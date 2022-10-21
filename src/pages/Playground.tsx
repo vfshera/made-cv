99574966ac3ff371e8640116e25e1a3efc5e0073
Franklin Shera <fshera96@gmail.com>
2022-10-21 04:15:00 +0300

    setup

---
+++ src/pages/Playground.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Playground = () => {
+  return (
+    <div className="playground">
+      <aside className="sidebar ">LEFT</aside>
+      <main className="view">Main Area</main>
+      <aside className="sidebar">RIGHT</aside>
+    </div>
+  );
+};
+
+export default Playground;
